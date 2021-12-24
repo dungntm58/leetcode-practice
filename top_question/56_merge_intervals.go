0231812289d64559d21d758764b329ca4a724579
dungntm58 <minhdung.uet.work@gmail.com>
2021-12-24 10:13:19 +0700

    56 (Update sorting implementation)

--- top_question/56_merge_intervals.go
+++ top_question/56_merge_intervals.go
@@ -17,7 +17,9 @@ func Merge(intervals [][]int) [][]int {
 	if n == 1 {
 		return intervals
 	}
-	sort.Sort(Intervals(intervals))
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
 	ans := [][]int{intervals[0]}
 	for i := 1; i < n; i++ {
 		lastAns := ans[len(ans)-1]
@@ -40,18 +42,4 @@ func Merge(intervals [][]int) [][]int {
 		}
 	}
 	return ans
-}
-
-type Intervals [][]int
-
-func (intervals Intervals) Len() int {
-	return len(intervals)
-}
-
-func (intervals Intervals) Less(i, j int) bool {
-	return intervals[i][0] < intervals[j][0]
-}
-
-func (intervals Intervals) Swap(i, j int) {
-	intervals[i], intervals[j] = intervals[j], intervals[i]
 }
