a0c447c7d60e471b13a1a166e3c96790092b2c1e
dungntm58 <minhdung.uet.work@gmail.com>
2022-03-25 03:13:02 +0700

    881

---
+++ top_question/881_boats_to_save_people.go
@@ -0,0 +1,33 @@
+package top_question
+
+import "sort"
+
+/// Boats to Save People
+// You are given an array people where people[i] is the weight of the ith person,
+// and an infinite number of boats where each boat can carry a maximum weight of limit.
+// Each boat carries at most two people at the same time,
+// provided the sum of the weight of those people is at most limit.
+// Return the minimum number of boats to carry every given person.
+// Input: people = [1,2], limit = 3
+// Output: 1
+// Explanation: 1 boat (1, 2)
+// Input: people = [3,2,2,1], limit = 3
+// Output: 3
+// Explanation: 3 boats (1, 2), (2) and (3)
+// Input: people = [3,5,3,4], limit = 5
+// Output: 4
+// Explanation: 4 boats (3), (3), (4), (5)
+
+func NumRescueBoats(people []int, limit int) int {
+	sort.Ints(people)
+	i, j := 0, len(people)-1
+	ans := 0
+	for i <= j {
+		ans++
+		if people[i]+people[j] <= limit {
+			i++
+		}
+		j--
+	}
+	return ans
+}
